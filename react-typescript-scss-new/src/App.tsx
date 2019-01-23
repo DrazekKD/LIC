d7c443ec747e0419ad19905c52d3b96194a22aa4
Kamil Drazkiewicz <k.drazkiewicz@getprintbox.com>
2019-01-23 15:36:20 +0100

    Material UI added

--- react-typescript-scss-new/src/App.tsx
+++ react-typescript-scss-new/src/App.tsx
@@ -1,10 +1,13 @@
+import Button from '@material-ui/core/Button';
 import * as React from 'react';
 import './App.scss';
-
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
+		  <Button variant="contained" color="primary">
+			  Hello World
+		  </Button>
       </div>
     );
   }
